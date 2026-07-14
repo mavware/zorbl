d9ceea876555f0700cbea39ae75af8454681ae48
Michael Greer <mlgreer1430@gmail.com>
2026-07-14 09:02:46 -0500

    Prepare for launch

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -12,8 +12,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
     <title>{{ $title ?? __('Something went wrong') }} · {{ $appName }}</title>
-    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
-    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
+{{--    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">--}}
+{{--    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">--}}
+    <link rel="icon" href="{{ asset('favicon-32x32.png') }}" type="image/png" sizes="32x32">
+    <link rel="icon" href="{{ asset('favicon-16x16.png') }}" type="image/png" sizes="16x16">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <meta name="theme-color" content="#0a0a0a">
     <style>
         :root { color-scheme: dark; }
